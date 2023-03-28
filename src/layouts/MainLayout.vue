df944229efff423bddb7cf8e244f62e99423d257
risachoi64 <risa.choi64@gmail.com>
2023-03-28 16:27:01 +0900

    Breakpoints

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -55,6 +55,12 @@ const linksList = [
     icon: 'school',
     to: '/spacing',
   },
+  {
+    title: 'Breakpoints',
+    caption: 'quasar.dev',
+    icon: 'school',
+    to: '/breakpoints',
+  },
 ];
 </script>
 
