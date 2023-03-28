617f4af0e05781218bd7becc57084e5493d84fa1
risachoi64 <risa.choi64@gmail.com>
2023-03-28 16:45:06 +0900

    HelperClasses

---
+++ src/pages/ClassesVariables.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="q-pa-md q-pa-md-xl">
+    <section class="q-mb-xl">
+      <div class="text-h5">Shadow</div>
+      <q-separator class="q-my-md" />
+      <div class="q-gutter-y-sm">
+        <div class="shadow-1 q-pa-sm">shadow-1</div>
+        <div class="shadow-7 q-pa-sm">shadow-7</div>
+        <div class="shadow-24 q-pa-sm">shadow-24</div>
+      </div>
+    </section>
+    <section class="q-mb-xl">
+      <div class="text-h5">Visibility</div>
+      <q-separator class="q-my-md" />
+      <div class="ellipsis">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta hic
+        asperiores suscipit provident magnam, magni nihil, illo sunt, sint
+        corrupti atque consectetur velit temporibus! Provident corrupti enim
+        voluptas omnis eveniet.
+      </div>
+      <hr />
+      <div class="ellipsis-2-lines cursor-pointer">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta hic
+        asperiores suscipit provident magnam, magni nihil, illo sunt, sint
+        corrupti atque consectetur velit temporibus! Provident corrupti enim
+        voluptas omnis eveniet.
+      </div>
+      <hr />
+      <div class="ellipsis-3-lines">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta hic
+        asperiores suscipit provident magnam, magni nihil, illo sunt, sint
+        corrupti atque consectetur velit temporibus! Provident corrupti enim
+        voluptas omnis eveniet.
+      </div>
+      <hr />
+    </section>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
